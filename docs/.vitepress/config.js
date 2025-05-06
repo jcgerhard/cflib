9b42534c8a6f8f0597a2e750481fffebd1b4cae2
jcgerhard <29855366+jcgerhard@users.noreply.github.com>
2025-05-06 11:57:38 +0200

    Refactor documentation structure: remove unused files, update sidebar links, and enhance last updated configuration

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,13 +5,6 @@ export default defineConfig({
     title: 'cflib',
     description: 'A tiny inconspicuous javascript library for Cloudflow',
     themeConfig: {
-        lastUpdated: {
-            text: 'Updated at',
-            formatOptions: {
-                dateStyle: 'full',
-                timeStyle: 'medium',
-            },
-        },
         search: {
             provider: 'local',
         },
@@ -20,18 +13,39 @@ export default defineConfig({
 
         sidebar: [
             { text: 'Introduction', link: '/introduction' },
-            { text: 'Quick Start', link: '/quickstart' },
+            { text: 'Installation', link: '/installation' },
             { text: 'Usage', link: '/usage' },
             {
                 text: 'Documentation',
                 collapsed: true,
-                items: [{ text: 'Colors', link: '/documentation/colors' }],
+                items: [
+                    { text: 'Colors', link: '/documentation/colors' },
+                    { text: 'Environment', link: '/documentation/environment' },
+                    { text: 'Metadata', link: '/documentation/metadata' },
+                    { text: 'RIP', link: '/documentation/rip' },
+                    { text: 'System', link: '/documentation/system' },
+                    { text: 'User', link: '/documentation/user' },
+                    { text: 'Utilities', link: '/documentation/utilities' },
+                ],
             },
         ],
         socialLinks: [{ icon: 'github', link: 'https://github.com/jcgerhard/cflib' }],
         footer: {
             message: 'Released under the MIT License.',
             copyright: 'Copyright © 2025-present Jan C. Gerhard',
+        },
+        lastUpdated: {
+            text: 'Updated at',
+            formatOptions: {
+                year: 'numeric',
+                month: 'numeric',
+                day: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: false,
+                timeZone: 'Europe/Berlin', // Use appropriate time zone
+                timeZoneName: 'short', // Adds time zone abbreviation
+            },
         },
     },
 });
